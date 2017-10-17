7aca94e8b517e41d0dd7879bbc9a3272896b7153
Juliette Pretot <hi@juliette.sh>
2017-10-17 07:52:32 +0200

    fixedparse

--- src/helperfunctions/EncodeEvent.js
+++ src/helperfunctions/EncodeEvent.js
@@ -16,7 +16,7 @@ function EncodeEvent(EventTime) {
 
 function DecodeEventtoUnixTime(EventEpoch2017MinutesB64) {
   // decode to decimal
-  EventEpoch2017MinutesB64 = EventEpoch2017MinutesB64.toString();
+  EventEpoch2017MinutesB64 = parseInt(EventEpoch2017MinutesB64, 36);
   var EventEpoch2017Minutes = parseInt(EventEpoch2017MinutesB64);
   var EventUnixMinutes = EventEpoch2017Minutes + 14832288;
   var EventUnixTime = Math.floor(EventUnixMinutes * 1e2);
