708aafe2afd97bbb6c16e57eb81a1aa8ded24162
Juliette Pretot <hi@juliette.sh>
2017-11-30 19:50:51 +0100

    added traling slash

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,7 @@ const { injectBabelPlugin } = require("react-app-rewired");
 module.exports = function override(config, env) {
   config = injectBabelPlugin("emotion", config);
   config = injectBabelPlugin(
-    ["babel-plugin-root-import", { rootPathSuffix: "src" }],
+    ["babel-plugin-root-import", { rootPathSuffix: "src/" }],
     config
   );
   return config;
