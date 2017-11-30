dd5dad7c70e5b9a0bd5159da0ed851f5f2f2ee47
Juliette Pretot <hi@juliette.sh>
2017-11-30 14:09:08 +0100

    adjusting margin of h1

--- src/ContentPages/AboutYourTimeZone.js
+++ src/ContentPages/AboutYourTimeZone.js
@@ -17,16 +17,7 @@ class AboutYourTimeZone extends React.Component {
           <span>
             yourtime.zone translates the time of an event into the visitor's
             local time. Great for webinars and live-streams.{" "}
-            <span id="credit">
-              Built by <a href="https://juliette.sh">Juliette Pretot</a>
-            </span>
           </span>
-          <ul className="introbulletpoints">
-            <li>Works with daylight saving time</li>
-            <li>
-              <Link to="/p/54g0/Google%20Pixel%20Event">Live example</Link>
-            </li>
-          </ul>
         </div>
         <Link to="/new" className="btn-class bigbutton">
           Create an event
