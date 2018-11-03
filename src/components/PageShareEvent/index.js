cb6073e998bf72dedfcea63b17b034188d46a3b0
Juliette Pretot <hi@juliette.sh>
2018-11-04 00:52:49 +0100

    refactor to react 16.7 with hooks

--- src/components/PageShareEvent/index.js
+++ src/components/PageShareEvent/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { Link } from 'react-router-dom'
 import ResponsiveBr from 'react-responsivebr'
@@ -7,41 +7,36 @@ import TextInput from '~/components/TextInput'
 import Button from '~/components/Button'
 import { parameterToTimestamp } from '~/helpers/timeParameter'
 
-class PageShareEvent extends React.Component {
-  state = {
-    isCopied: false
-  }
+const PageShareEvent = props => {
+  const [isCopied, setIsCopied] = useState(false)
+  const { encodedTime, name } = props.match.params
+  const path = `/e/${encodedTime}${name ? '/' + name : ''}`
+  const url = window.location.origin + path
 
-  onCopy = () => this.setState({ isCopied: true })
-
-  handleInputClick(event) {
-    event.target.select()
-    event.preventDefault()
-  }
-
-  render() {
-    const { encodedTime, name } = this.props.match.params
-    const path = `/e/${encodedTime}${name ? '/' + name : ''}`
-    const url = window.location.origin + path
-
-    return (
-      <LayoutGradient backgroundTime={parameterToTimestamp(encodedTime)}>
-        <h1>{this.state.isCopied ? 'Copied!' : 'Nicely done.'}</h1>
-        <p>
-          Now, simply share the link below.
-          <ResponsiveBr minWidth="500" />
-          It will display <Link to={path}>the event</Link> in the local time of
-          whoever visits it.
-        </p>
-        <TextInput onClick={this.handleInputClick} value={url} readOnly />
-        <CopyToClipboard text={url} onCopy={this.onCopy}>
-          <Button as="button" data-clipboard-target="#copy-input">
-            Copy
-          </Button>
-        </CopyToClipboard>
-      </LayoutGradient>
-    )
-  }
+  return (
+    <LayoutGradient backgroundTime={parameterToTimestamp(encodedTime)}>
+      <h1>{isCopied ? 'Copied!' : 'Nicely done.'}</h1>
+      <p>
+        Now, simply share the link below.
+        <ResponsiveBr minWidth="500" />
+        It will display <Link to={path}>the event</Link> in the local time of
+        whoever visits it.
+      </p>
+      <TextInput
+        onClick={event => {
+          event.target.select()
+          event.preventDefault()
+        }}
+        value={url}
+        readOnly
+      />
+      <CopyToClipboard text={url} onCopy={() => setIsCopied('true')}>
+        <Button as="button" data-clipboard-target="#copy-input">
+          Copy
+        </Button>
+      </CopyToClipboard>
+    </LayoutGradient>
+  )
 }
 
 export default PageShareEvent
