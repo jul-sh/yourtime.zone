8bd55d9397b3d271878f53f2ee393d2e8c9e4afa
Juliette Pretot <hi@juliette.sh>
2018-11-04 00:52:49 +0100

    refactor to react 16.7 with hooks

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,51 +11,41 @@ import PageCreateEvent from '~/components/PageCreateEvent'
 import PageShareEvent from '~/components/PageShareEvent'
 import PageEvent from '~/components/PageEvent'
 
-class App extends Component {
-  scrollToTop = () => window.scrollTo(0, 0)
-
-  render() {
-    return (
-      <Router onUpdate={this.scrollToTop}>
-        <ResetScrollPosition>
-          <Switch>
-            <TimezoneState>
-              {timezoneState => (
-                <>
-                  <Route
-                    exact
-                    path="/"
-                    render={props => (
-                      <PageAbout {...props} {...timezoneState} />
-                    )}
-                  />
-                  <Route
-                    path="/new/:timezone?/"
-                    render={props => (
-                      <PageCreateEvent {...props} {...timezoneState} />
-                    )}
-                  />
-                  <Route
-                    path="/share/:encodedTime?/:name?/"
-                    render={props => (
-                      <PageShareEvent {...props} {...timezoneState} />
-                    )}
-                  />
-                  <Route
-                    path="/e/:encodedTime?/:name?/"
-                    render={props => (
-                      <PageEvent {...props} {...timezoneState} />
-                    )}
-                  />
-                </>
-              )}
-            </TimezoneState>
-          </Switch>
-        </ResetScrollPosition>
-      </Router>
-    )
-  }
-}
+const App = props => (
+  <Router>
+    <ResetScrollPosition>
+      <Switch>
+        <TimezoneState>
+          {timezoneState => (
+            <>
+              <Route
+                exact
+                path="/"
+                render={props => <PageAbout {...props} {...timezoneState} />}
+              />
+              <Route
+                path="/new/:timezone?/"
+                render={props => (
+                  <PageCreateEvent {...props} {...timezoneState} />
+                )}
+              />
+              <Route
+                path="/share/:encodedTime?/:name?/"
+                render={props => (
+                  <PageShareEvent {...props} {...timezoneState} />
+                )}
+              />
+              <Route
+                path="/e/:encodedTime?/:name?/"
+                render={props => <PageEvent {...props} {...timezoneState} />}
+              />
+            </>
+          )}
+        </TimezoneState>
+      </Switch>
+    </ResetScrollPosition>
+  </Router>
+)
 
 const ResetScrollPosition = withRouter(
   class extends Component {
--- src/components/PageCreateEvent/index.js
+++ src/components/PageCreateEvent/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { withRouter } from 'react-router-dom'
 import { parse as sherlock } from 'sherlockjs'
 import { DateTime } from 'luxon'
@@ -8,75 +8,63 @@ import LayoutGradient from '~/components/LayoutGradient'
 import TextInput from '~/components/TextInput'
 import Button from '~/components/Button'
 
-class PageCreateEvent extends React.Component {
-  state = {
-    eventTime: '',
-    eventTimeError: '',
-    eventName: ''
-  }
+const PageCreateEvent = props => {
+  const [eventTime, setEventTime] = useState('')
+  const [eventName, setEventName] = useState('')
+  const [eventTimeError, setEventTimeError] = useState('')
+  const timezone = formatTimezoneName(props.timezone)
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault()
-
-    const { eventTime, eventName } = this.state
     const { startDate } = sherlock(eventTime)
     const eventTimestamp = DateTime.fromJSDate(startDate, {
-      zone: this.props.timezone
+      zone: props.timezone
     }).toMillis()
 
     if (startDate) {
-      this.props.history.push({
+      props.history.push({
         pathname: `/share/${timestampToParameter(eventTimestamp)}/${eventName}`
       })
     } else {
-      this.setState({
-        eventTimeError: 'Please double check the format'
-      })
+      setEventTimeError('Please double check the format')
     }
   }
 
-  handleTimeChange = ({ target: { value } }) => {
-    this.setState({ eventTime: value, eventTimeError: '' })
-  }
-
-  handleNameChange = ({ target: { value } }) =>
-    this.setState({ eventName: value })
-
-  render() {
-    const timezone = formatTimezoneName(this.props.timezone)
-
-    return (
-      <LayoutGradient backgroundTime={Date.now()}>
-        <form onSubmit={this.handleSubmit}>
-          <h2>When is the event?</h2>
-          <p>
-            In your local time zone ({timezone}
-            ).
-          </p>
-          <TextInput
-            required
-            name="eventTime"
-            value={this.state.eventTime}
-            onChange={this.handleTimeChange}
-            placeholder="e.g. Wednesday at 7pm"
-            error={this.state.eventTimeError}
-            marginBottom="30px"
-          />
-          <h2>{"What's the name of the event?"}</h2>
-          <TextInput
-            name="eventName"
-            value={this.state.eventName}
-            onChange={this.handleNameChange}
-            placeholder="e.g. Juliette's Webinar"
-            marginBottom="40px"
-          />
-          <br />
-          <Button as="button" type="submit">
-            Create Event
-          </Button>
-        </form>
-      </LayoutGradient>
-    )
-  }
+  return (
+    <LayoutGradient backgroundTime={Date.now()}>
+      <form onSubmit={handleSubmit}>
+        <h2>When is the event?</h2>
+        <p>
+          In your local time zone ({timezone}
+          ).
+        </p>
+        <TextInput
+          required
+          name="eventTime"
+          value={eventTime}
+          onChange={({ target: { value } }) => {
+            setEventTime(value)
+            setEventTimeError('')
+          }}
+          placeholder="e.g. Wednesday at 7pm"
+          error={eventTimeError}
+          marginBottom="30px"
+        />
+        <h2>{"What's the name of the event?"}</h2>
+        <TextInput
+          name="eventName"
+          value={eventName}
+          onChange={({ target: { value } }) => setEventName(value)}
+          placeholder="e.g. Juliette's Webinar"
+          marginBottom="40px"
+        />
+        <br />
+        <Button as="button" type="submit">
+          Create Event
+        </Button>
+      </form>
+    </LayoutGradient>
+  )
 }
+
 export default withRouter(PageCreateEvent)
